2ff47d5f8814894ccad2fa4d8d5d0af4c5b4691e
Nayema <26700552+nayema@users.noreply.github.com>
2026-01-18 22:26:38 -0500

    fix tests with jest-dom to test html elements

--- __tests__/components/NavigationButtons.test.tsx
+++ __tests__/components/NavigationButtons.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
 import { NavigationButtons } from '@/components/NavigationButtons'
 
 // Mock Next.js Link
